e48b26f70cf50084b89f031f89000d188da6ec75
Anish <anishbajpai91@outlook.com>
2023-05-03 21:56:42 -0400

    added comments for Jay

--- mongoTest.py
+++ mongoTest.py
@@ -31,40 +31,42 @@ st.write(pd.DataFrame(mycol.find({})))
 # projecting so that id doesnt show
 st.write(pd.DataFrame(mycol.find({}, projection = {'_id': 0})))
 
-# text input
+# user inputs
 name = st.text_input('name', "Name")
 town = st.text_input('town', "Town")
-
 age = st.number_input('age', 0, 100)
-
 st.write(name, town, age)
 
-### function to insert document into database
+# function to insert document into database
 def mongoInput(name, town, age):
+    # db function to put the document into the db
     inserted = mycol.insert_one({'name':name, 'city':town, 'age':age})
     return inserted
+
 # button to insert
 if st.button('insert'):
+    # call on the input function to put the document into the db
     st.write(mongoInput(name, town, age))
+    # refresh the page so that the data frame displays are updated
     st.experimental_rerun()
 
-# testing to display a graph 
+# button to display the graph
 if st.button('display'):
+    # db qurry to get all the name and age fields
     nameAge = mycol.find({}, projection = {'name':1, 'age':1, '_id':0})
+    
+    # array to hold the dicts returned by the query
     arr = []
     for doc in nameAge:
         arr.append(doc)
-    #st.write(pd.DataFrame(nameAge))
 
+    # processing the data to make it usable 
     data = {'names': [x['name'] for x in arr], 'ages':[x['age'] for x in arr]}
-    #print(data)
 
-    # print('name age') 
-    # print(arr)
-
+    # make a figure and then add the plots in ax
     fig, ax = plt.subplots()
     ax.scatter(data['names'], data['ages'])
-    fig.show()
+    # use streamlit to show the figure
     st.pyplot(fig)
 
 
